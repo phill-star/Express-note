b18ee8d7f9f076b7c05064b44a871bd2e0804a49
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-06-26 07:39:53 -0600

    editedServerfile

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = (process.env.PORT || 8080);
-
+class App {
+  constructor() {
+    this.app = express();
+    this.PORT = process.env.PORT || 8080;
+  }}
