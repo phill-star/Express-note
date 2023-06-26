1bb934215710a2e397c996df6f25869be7265e28
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-06-26 14:56:08 -0600

    addIfElseStatement

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -10,3 +10,16 @@ router.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+  router.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    if (req.body && noteId) {
+      console.info(`${req.method} request received for note ${noteId}.`);
+      const foundNote = notes.find((note) => note.note_id === noteId);
+      if (foundNote) {
+        res.json(foundNote);
+      } else {
+        res.json('Note ID not found');
+      }
+    }
+  });
