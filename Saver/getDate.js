36c19756fa6a335ea7c4dc9ba8beeabf7dd6f4be
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-06-26 21:21:46 -0600

    addedTimefunction

--- Saver/getDate.js
+++ Saver/getDate.js
@@ -9,5 +9,17 @@ function getDate() {
     const date = newDate.getDate();
     let hour = newDate.getHours();
     
+  // Places 0 in front of hours less than 10 (i.e., 06:00 instead of 6:00).
+  hour = (hour < 10) ? `0${hour}` : hour;
+  
+  let min = newDate.getMinutes();
+
+  // Places 0 in front of minutes less than 10 (i.e., 06:00 instead of 06:0).
+  min = (min < 10) ? `0${min}` : min;
+
+  const time = `${date} ${month} ${year} ${hour}:${min}`;
+  return time;
 }
+
+module.exports = getDate;
   
