02a6ce949912329535aacf09ece95e25852bbd31
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-06-26 21:44:38 -0600

    addAppend

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -3,3 +3,16 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 const writeToFile = util.promisify(fs.writeFile);
+
+const readAndAppend = async (content, file) => {
+    try {
+      const data = await readFromFile(file);
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      await writeToFile(file, parsedData);
+      console.info(`\nData appended to ${file}`);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
