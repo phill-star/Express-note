b9f8fce04a1c53625d55437f2c9e47b4e1ae505a
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-06-29 21:10:03 -0600

    fixedSavederror

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,66 +1,101 @@
 const express = require('express');
-const fs = require('fs');
-const uuid = require('uuid');
-const getDate = require('../routes/getDate.js');
-const notes = require('../db/db.json');
-
 const router = express.Router();
+const fs = require('fs');
+const { promisify } = require('util');
+const uuid = require('../helpers/uuid');
+const getDate = require('../helpers/getDate');
 
-router.get('/', (req, res) => {
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const dbFilePath = './db/db.json';
+
+const readFromFile = async () => {
+  try {
+    const data = await readFile(dbFilePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading from file:', error);
+    throw error;
+  }
+};
+
+const writeToFile = async (data) => {
+  try {
+    await writeFile(dbFilePath, JSON.stringify(data, null, 4));
+    console.info(`Data written to ${dbFilePath}`);
+  } catch (error) {
+    console.error('Error writing to file:', error);
+    throw error;
+  }
+};
+
+router.get('/', async (req, res) => {
+  try {
     console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
+    const data = await readFromFile();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json('Internal server error');
+  }
+});
 
-  router.get('/:note_id', (req, res) => {
+router.get('/:note_id', async (req, res) => {
+  try {
     const noteId = req.params.note_id;
-    if (req.body && noteId) {
-      console.info(`${req.method} request received for note ${noteId}.`);
-      const foundNote = notes.find((note) => note.note_id === noteId);
-      if (foundNote) {
-        res.json(foundNote);
-      } else {
-        res.json('Note ID not found');
-      }
+    console.info(`${req.method} request received for note ${noteId}.`);
+    const data = await readFromFile();
+    const note = data.find((note) => note.note_id === noteId);
+    if (note) {
+      res.json(note);
+    } else {
+      res.json('Note ID not found');
     }
-  });
-  
-  router.delete('/:note_id', (req, res, next) => {
+  } catch (error) {
+    res.status(500).json('Internal server error');
+  }
+});
+
+router.delete('/:note_id', async (req, res) => {
+  try {
     const noteId = req.params.note_id;
     console.info(`${req.method} request received for note id ${noteId}.`);
-    for (let i = 0; i < notes.length; i++) {
-      const currentNote = notes[i];
-      if (currentNote.note_id === noteId) {
-        res.json(`${currentNote.title} has been deleted`);
-        notes.splice(i, 1);
-        console.log(notes);
-        readAndOverwrite(notes, './db/db.json');
-        return;
-      }
+    const data = await readFromFile();
+    const noteIndex = data.findIndex((note) => note.note_id === noteId);
+    if (noteIndex !== -1) {
+      const deletedNote = data.splice(noteIndex, 1)[0];
+      await writeToFile(data);
+      res.json(`${deletedNote.title} has been deleted`);
+    } else {
+      res.json('Note ID not found');
     }
-  });
-  router.post('/', (req, res) => {
+  } catch (error) {
+    res.status(500).json('Internal server error');
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
     console.info(`${req.method} request received to add a note.`);
+
     const { title, text } = req.body;
 
-    if (req.body) {
+    if (title && text) {
       const newNote = {
         title,
         date: getDate(),
         text,
         note_id: uuid(),
       };
-      notes.push(newNote);
-      fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        if (err) {
-          console.error('Error writing to file:', err);
-          res.status(500).json('Internal server error');
-        } else {
-          res.json(`Note added successfully. ID: ${newNote.note_id}`);
-        }
-      });
+      const data = await readFromFile();
+      data.push(newNote);
+      await writeToFile(data);
+      res.json(`Note added successfully. ID: ${newNote.note_id}`);
     } else {
-      res.status(400).json('Error in adding note. Please try again.');
+      res.status(400).json('Error in adding note. Please provide both title and text.');
     }
-  });
-  
-  module.exports = router;
+  } catch (error) {
+    res.status(500).json('Internal server error');
+  }
+});
+
+module.exports = router;
