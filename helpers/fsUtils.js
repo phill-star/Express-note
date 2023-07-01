51729e2576d0953145b27dba7629491289fec0a9
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-07-01 09:17:44 -0600

    modifiedAppendHandle

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,33 +1,49 @@
 const fs = require('fs');
 const util = require('util');
 
-const readFromFile = util.promisify(fs.readFile);
-const writeToFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const readFromFile = async (file) => {
+  try {
+    const data = await readFile(file, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+const writeToFile = async (destination, content) => {
+  try {
+    await writeFile(destination, JSON.stringify(content, null, 4));
+    console.info(`\nData written to ${destination}`);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const readAndAppend = async (content, file) => {
-    try {
-      const data = await readFromFile(file);
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      await writeToFile(file, parsedData);
-      console.info(`\nData appended to ${file}`);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
-  const readAndOverwrite = async (content, file) => {
-    try {
-      await writeToFile(file, content);
-      console.info(`\nData overwritten in ${file}`);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
-  module.exports = {
-    readFromFile,
-    readAndAppend,
-    readAndOverwrite
-  };
-  
+  try {
+    const data = await readFromFile(file);
+    const parsedData = Array.isArray(data) ? data : [];
+    parsedData.push(content);
+    await writeToFile(file, parsedData);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const readAndOverwrite = async (content, file) => {
+  try {
+    await writeToFile(file, content);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = {
+  readFromFile,
+  readAndAppend,
+  readAndOverwrite
+};
