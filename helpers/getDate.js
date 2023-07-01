a45a75cffe1bf30dffd8d715d660f23bba18c24d
Mohamed Hussein <mohamedhussein@Mohameds-MacBook-Pro.local>
2023-07-01 07:49:02 -0600

    modifiedDate.js

--- helpers/getDate.js
+++ helpers/getDate.js
@@ -1,25 +1,24 @@
 function getDate() {
-    const newDate = new Date();
-    const months = [
-      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    ];
-    const year = newDate.getFullYear();
-    const month = months[newDate.getMonth()];
-    const date = newDate.getDate();
-    let hour = newDate.getHours();
-    
-  // Places 0 in front of hours less than 10 (i.e., 06:00 instead of 6:00).
-  hour = (hour < 10) ? `0${hour}` : hour;
-  
+  const newDate = new Date();
+  const months = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+  const year = newDate.getFullYear();
+  const month = months[newDate.getMonth()];
+  const date = newDate.getDate();
+  let hour = newDate.getHours();
   let min = newDate.getMinutes();
 
-  // Places 0 in front of minutes less than 10 (i.e., 06:00 instead of 06:0).
-  min = (min < 10) ? `0${min}` : min;
+  hour = padZero(hour);
+  min = padZero(min);
 
   const time = `${date} ${month} ${year} ${hour}:${min}`;
   return time;
 }
 
+function padZero(value) {
+  return (value < 10) ? `0${value}` : value;
+}
+
 module.exports = getDate;
-  
